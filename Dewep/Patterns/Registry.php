664ed884eef235eedb60df320dbf21826b8ce2ec
Mikhail Knyazhev <markus621@gmail.com>
2017-11-08 07:27:15 +0300

    Patterns

---
+++ Dewep/Patterns/Registry.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Mikhail Knyazhev <markus621@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Dewep\Patterns;
+
+use Dewep\Exception\RuntimeException;
+
+/**
+ * Description of Registry
+ *
+ * @author Mikhail Knyazhev <markus621@gmail.com>
+ */
+abstract class Registry
+{
+
+    protected static $__registry = array();
+
+    /**
+     *
+     * @param string $key
+     * @param type $value
+     * @throws RuntimeException
+     */
+    final public static function set(string $key, $value)
+    {
+        self::$__registry[$key] = $value;
+    }
+
+    /**
+     *
+     * @param string $key
+     * @return type
+     */
+    final public static function get(string $key)
+    {
+        return self::$__registry[$key] ?? null;
+    }
+
+    /**
+     *
+     * @param string $key
+     */
+    final public static function remove(string $key)
+    {
+        unset(self::$__registry[$key]);
+    }
+
+}
---
+++ Dewep/Patterns/SingletonRegistry.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Mikhail Knyazhev <markus621@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Dewep\Patterns;
+
+/**
+ * Description of SingletonRegistry
+ *
+ * @author Mikhail Knyazhev <markus621@gmail.com>
+ */
+abstract class SingletonRegistry
+{
+
+    static private $_instance = null;
+    private $_registry = array();
+
+    final public static function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self;
+        }
+
+        return self::$_instance;
+    }
+
+    final public static function set($key, $object)
+    {
+        self::getInstance()->_registry[$key] = $object;
+    }
+
+    final public static function get($key)
+    {
+        return self::getInstance()->_registry[$key] ?? null;
+    }
+
+    private function __wakeup()
+    {
+
+    }
+
+    private function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+
+    }
+
+}
